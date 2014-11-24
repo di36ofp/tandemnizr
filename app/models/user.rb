b63835c392849e63d8272e9582316f821dda0d25
Diego <info@diego-suarez.es>
2014-11-24 14:38:01 +0100

    add encrypted method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,30 @@
 class User < ActiveRecord::Base
 
-  validates :name, presence:    true,
-                   uniqueness:  true,
+  before_save :encrypt_password
+
+  alpha_num_regex = /\A[\w\s*]+\Z/
+  email_regex = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
+  strong_password_regex = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/
+
+  validates :name, uniqueness:  true,
                    length:      { maximum: 30 },
-                   format:      { with: /\A[\w\s*]+\Z/ }
+                   format:      { with: alpha_num_regex }
   validates :email, presence:   true,
                    uniqueness:  true,
-                   format:      { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ }
+                   format:      { with: email_regex }
   validates :password, presence: true,
-                   format:      { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/ }
-  validates :nationality, presence: true
-  validates :city, presence: true
-  validates :cp, presence: true,
-                  :inclusion => 00000..99999
-  validates :age, presence: true,
-                  :inclusion => 18..99
-  validates :career, presence: true
-  validates :photo, presence: true
-  validates :description, presence: true
+                      :confirmation => true,
+                      format: { with: strong_password_regex }
+  #validates :nationality
+  #validates :city
+  validates :cp, :inclusion => 00000..99999
+  validates :age, :inclusion => 18..99
+  #validates :career
+  #validates :photo
+  #validates :description
+
+  private
+  def encrypt_password
+    self.password = encrypt(password)
+  end
 end
