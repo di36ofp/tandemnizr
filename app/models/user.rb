1dab1054d753aabeabc66d77d25511e09b25c7db
Diego <info@diego-suarez.es>
2014-11-24 16:57:45 +0100

    add validate on create, user, and readonly params to email<2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,52 @@
 class User < ActiveRecord::Base
-  require 'digest'
+
+  attr_accessor :name, :email
+
+  attr_readonly :email
 
   before_save :encrypt_password
 
-  alpha_num_regex = /\A[\w\s*]+\Z/
-  email_regex = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/
-  strong_password_regex = /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/
-
-  validates :name, uniqueness:  true,
-                   length:      { maximum: 30 },
-                   format:      { with: alpha_num_regex }
-  validates :email, presence:   true,
+  validates :name, allow_blank:  true,
                    uniqueness:  true,
-                   format:      { with: email_regex }
+                   length:      { maximum: 30 },
+                   format:      { with: /\A[\w\s*]+\Z/ }
+  validates :email, presence:   true, if: :should_validate?,
+                   uniqueness:  true,
+                   format:      { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ ,
+                                  message: 'The format of email is invalid'}
   validates :password, presence: true,
                       :confirmation => true,
-                      format: { with: strong_password_regex }
+                      format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/ ,
+                                message: 'Must contain at least 8 chars, a majuscule, minuscule, number and special sign'}
   #validates :nationality
   #validates :city
-  validates :cp, :allow_blank => true,
-                  :inclusion => 00000..99999
-  validates :age,  :allow_blank => true,
-                  :inclusion => 18..99
+  validates :cp, allow_blank:  true,
+                :inclusion => 00000..99999
+  validates :age, allow_blank:  true,
+                :inclusion => 18..99
   #validates :career
   #validates :photo
   #validates :description
 
+  def has_password?(submitted_password)
+    password == encrypt(submitted_password)
+  end
+
   private
+
+  def should_validate?
+    new_record? || email.present?
+  end
+
   def encrypt_password
-    self.password = Digest::SHA2.hexdigest(password)
+    self.password = secure_hash(password)
+  end
+
+  def encrypt(string)
+    secure_hash(string)
+  end
+
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
   end
 end
