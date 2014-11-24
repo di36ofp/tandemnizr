e51b7b823de3edb203b1f564d30550d4ea88f3d9
Diego <info@diego-suarez.es>
2014-11-24 19:20:33 +0100

    add validation for password on create and on update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,39 +25,33 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      flash[:success] = "User successfully created"
+      redirect_to @user
+    else
+      flash[:success] = "User can't be created"
+      render 'new'
     end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = "User successfully created"
+      redirect_to @user
+    else
+      flash[:success] = "User can't be updated"
+      render 'edit'
     end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if @user.destroy
+      flash[:success] = "User successfully created"
+      redirect_to users_url
     end
   end
 
@@ -69,6 +63,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :nationality, :city, :cp, :age, :career, :photo, :description)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :nationality, :city, :cp, :age, :career, :photo, :description)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
 
-  attr_accessor :name, :email
+  #attr_accessor :password, :password_confirmation
 
-  attr_readonly :email
+  before_save(on: :create) do
+    :is_equal? if attribute_present?("password")
+  end
 
   before_save :encrypt_password
 
@@ -10,13 +12,14 @@ class User < ActiveRecord::Base
                    uniqueness:  true,
                    length:      { maximum: 30 },
                    format:      { with: /\A[\w\s*]+\Z/ }
-  validates :email, presence:   true, if: :should_validate?,
-                   uniqueness:  true,
-                   format:      { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ ,
+  validates :email, presence:   true,
+                    uniqueness:  true,
+                    format:      { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/ ,
                                   message: 'The format of email is invalid'}
-  validates :password, presence: true,
-                      :confirmation => true,
-                      format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/ ,
+  validates :password, confirmation: true,
+                       on: :create,
+                       presence: true,
+                       format: { with: /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}/ ,
                                 message: 'Must contain at least 8 chars, a majuscule, minuscule, number and special sign'}
   #validates :nationality
   #validates :city
@@ -28,11 +31,21 @@ class User < ActiveRecord::Base
   #validates :photo
   #validates :description
 
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
   def has_password?(submitted_password)
     password == encrypt(submitted_password)
   end
 
   private
+
+  def is_equal?
+      password == password_confirmation
+  end
 
   def should_validate?
     new_record? || email.present?
