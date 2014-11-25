6194c3c806b279e7864f73ec8032a5808e94a19e
Diego <info@diego-suarez.es>
2014-11-25 10:30:50 +0100

    create views for languages

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,17 @@
 class LanguagesController < ApplicationController
+
+  def index
+    @user = set_user
+    @languages = @user.languages
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def user_params
+    params.require(:language).permit(:language, :user_id)
+  end
 end
