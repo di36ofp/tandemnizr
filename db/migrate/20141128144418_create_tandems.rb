04ab0457cb82b848485078195139a3d440a15f54
Diego <info@diego-suarez.es>
2014-11-29 01:15:41 +0100

    add post links to languages to create tandems

--- db/migrate/20141128144418_create_tandems.rb
+++
@@ -1,8 +0,0 @@
-class CreateTandems < ActiveRecord::Migration
-  def change
-    create_table :tandems do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20141128144439_create_participations.rb
+++
@@ -1,8 +0,0 @@
-class CreateParticipations < ActiveRecord::Migration
-  def change
-    create_table :participations do |t|
-
-      t.timestamps
-    end
-  end
-end
