7827bc41435b2c8e5e8f27f0c9294c0a61209cbe
Diego <info@diego-suarez.es>
2014-12-05 00:24:55 +0100

    fix files loading tree in asset javascript

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,56 +1,58 @@
-var map;
-var barcelona = new google.maps.LatLng(41.395603613998205, 2.157095799999979);
+if (typeof google != 'undefined' && google != 'null') {
+  var map;
+  var barcelona = new google.maps.LatLng(41.395603613998205, 2.157095799999979);
 
-var MY_MAPTYPE_ID = 'custom_style';
+  var MY_MAPTYPE_ID = 'custom_style';
 
-function initialize() {
+  function initialize() {
 
-  var featureOpts = [
-    {
-      elementType: 'labels',
-      stylers: [
-        { "visibility": "off" }
-      ]
-    },
-    {
-      featureType: 'water',
-      stylers: [
-        { color: '#9ECEB4' }
-      ]
-    }
-  ];
+    var featureOpts = [
+      {
+        elementType: 'labels',
+        stylers: [
+          { "visibility": "on" }
+        ]
+      },
+      {
+        featureType: 'water',
+        stylers: [
+          { color: '#9ECEB4' }
+        ]
+      }
+    ];
 
-  var mapOptions = {
-    zoom: 15,
-    center: barcelona,
-    mapTypeControlOptions: {
-      mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
-    },
-    mapTypeId: MY_MAPTYPE_ID
-  };
+    var mapOptions = {
+      zoom: 15,
+      center: barcelona,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+      },
+      mapTypeId: MY_MAPTYPE_ID
+    };
 
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
 
-  // var image = 'https://dl.dropboxusercontent.com/u/79814994/cdn.images/logo_hotel_map.png';
-  // var hotelMarker = new google.maps.Marker({
-  //     position: barcelona,
-  //     map: map,
-  //     icon: image
-  // });
+    // var image = 'https://dl.dropboxusercontent.com/u/79814994/cdn.images/logo_hotel_map.png';
+    // var hotelMarker = new google.maps.Marker({
+    //     position: barcelona,
+    //     map: map,
+    //     icon: image
+    // });
 
-  var styledMapOptions = {
-    name: 'Custom Style'
-  };
+    var styledMapOptions = {
+      name: 'Custom Style'
+    };
 
-  var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+    var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
 
-  map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+    map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-
-//////////////////////////////////////////
 var map = document.getElementById('map-canvas');
 
 function loadMarkers() {
